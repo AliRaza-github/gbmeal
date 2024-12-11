35a60e62729cfe03944922df3582723b29355073
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-12-11 15:54:20 +0500

    Hydration error resolved

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add your configuration options here
+};
+
+export default nextConfig;
